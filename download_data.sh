9a58e7344f71fea203fd975277861e9aad7004e5
Dominik Stammbach <dominik.stammbach@bluewin.ch>
2020-02-18 17:46:54 +0100

    quick fix

---
+++ download_data.sh
@@ -0,0 +1,24 @@
+# datasets
+# CLEF 2018 CheckThat! dataset
+# not needed since subset of CLEF 2019 CheckThat!
+# git clone https://github.com/clef2018-factchecking/clef2018-factchecking.git
+
+
+# CLEF 2019 CheckThat! dataset
+git clone https://github.com/apepa/clef2019-factchecking-task1.git
+# ClaimRank
+git clone https://github.com/apepa/claim-rank.git
+# FakeNewsNet
+git clone https://github.com/KaiDMML/FakeNewsNet.git
+# ClaimBuster
+wget -O ClaimBuster_data.zip https://ndownloader.figshare.com/articles/11635293/versions/1
+unzip ClaimBuster_data.zip -d claim_buster_data
+# MultiFC
+wget -O MultiFC_data.zip https://competitions.codalab.org/my/datasets/download/4db8bf21-def7-4a86-99f5-7b23d5691bb3
+unzip MultiFC_data.zip -d MultiFC_data
+
+
+# BERT
+wget -O bert_base_cased.zip https://storage.googleapis.com/bert_models/2018_10_18/cased_L-12_H-768_A-12.zip
+unzip bert_base_cased.zip -d bert_model
+
