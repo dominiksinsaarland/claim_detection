7b3440f34e9dd084e7d6bf3865c58331f7e6578c
dominiksinsaarland <dominik.stammbach@bluewin.ch>
2020-02-19 16:47:44 +0100

    roberta input

--- get_roberta_input.py
+++ get_roberta_input.py
@@ -11,6 +11,14 @@ if __name__ == "__main__":
 			outfile.write(sentence + "\t" + str(label) + "\n")
 
 	with open("dev_clef_2019.txt", "w") as outfile:
-		for sentence, label in train:
+		for sentence, label in dev:
 			outfile.write(sentence + "\t" + str(label) + "\n")
+
+
+	# bsub -W 2 -n 1 -R "rusage[mem=4096,scratch=8192]" -B -N python get_roberta_input.py
+
+	#test tensorflow GPU
+	# bsub -n 1 -R "rusage[mem=12800,ngpus_excl_p=1]" python test.py 
+
+
 
