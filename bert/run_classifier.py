bcc851c8b3e172a03a5b95ede59a71c22ada9239
dominiksinsaarland <dominik.stammbach@bluewin.ch>
2020-02-19 17:39:07 +0100

    quick fix

--- bert/run_classifier.py
+++ bert/run_classifier.py
@@ -254,6 +254,7 @@ class XnliProcessor(DataProcessor):
 
 class CLEF2019Processor(DataProcessor):
   def get_train_examples(self, data_dir):
+    return self._create_examples(
         self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
   def get_dev_examples(self, data_dir):
     """See base class."""
