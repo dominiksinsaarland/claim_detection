f24212274a856307e3dcc115f35601a7a07c7372
dominiksinsaarland <dominik.stammbach@bluewin.ch>
2020-02-20 12:41:49 +0100

    train on clef+claimbuster

--- get_bert_input_files.py
+++ get_bert_input_files.py
@@ -8,11 +8,16 @@ if __name__ == "__main__":
 	train, dev = CLEF_Loader.get_examples()
 	path_outfiles = "bert_input_files"
 	path_data = "clef2019-factchecking-task1/data/training/"
+	block_set = set([x[0] for x in dev])
+	train.extend(utils.ClaimBuster_Loader().get_examples(block_set=block_set))
+
 	try:
 		os.mkdir(path_outfiles)
 	except:
 		pass
-	with open("train_clef_2019.txt", "w") as outfile:
+
+
+	with open(os.path.join(path_outfiles, "train.txt"), "w") as outfile:
 		for sentence, label in train:
 			outfile.write(sentence + "\t" + str(label) + "\n")
 
@@ -34,6 +39,9 @@ if __name__ == "__main__":
 
 	#test tensorflow GPU
 	# bsub -n 1 -R "rusage[mem=12800,ngpus_excl_p=1]" python test.py 
+
+	# get bert input
+	# bsub -W 2 -n 1 -R "rusage[mem=4096,scratch=8192]" -B -N python get_bert_input_files.py
 
 
 
