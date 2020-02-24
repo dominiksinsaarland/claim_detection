1fc959ff8b4c377422788ba3764de102805cb0e1
dominiksinsaarland <dominik.stammbach@bluewin.ch>
2020-02-24 11:53:52 +0100

    eval joint training

--- get_bert_input_files.py
+++ get_bert_input_files.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 	path_outfiles = "bert_input_files"
 	path_data = "clef2019-factchecking-task1/data/training/"
 	block_set = set([x[0] for x in dev])
-	train.extend(utils.ClaimBuster_Loader().get_examples(block_set=block_set))
+	#train.extend(utils.ClaimBuster_Loader().get_examples(block_set=block_set))
 
 	try:
 		os.mkdir(path_outfiles)
@@ -17,13 +17,20 @@ if __name__ == "__main__":
 		pass
 
 
-	with open(os.path.join(path_outfiles, "train.txt"), "w") as outfile:
+	with open(os.path.join(path_outfiles, "CLEF_train_input.txt"), "w") as outfile:
 		for sentence, label in train:
 			outfile.write(sentence + "\t" + str(label) + "\n")
 
 	with open(os.path.join(path_outfiles, "dev.txt"), "w") as outfile:
 		for sentence, label in dev:
 			outfile.write(sentence + "\t" + str(label) + "\n")
+
+	train.extend(utils.ClaimBuster_Loader().get_examples(block_set=block_set))
+	with open(os.path.join(path_outfiles, "CLEF_AND_CLAIMBUSTER_train_input.txt"), "w") as outfile:
+		for sentence, label in train:
+			outfile.write(sentence + "\t" + str(label) + "\n")
+
+
 
 	dev_fns = ["20161019_3pres.tsv", "20160414_9dem.tsv", "20180916_trump_miami.tsv", "20170928_trump_tax.tsv", "20182601_trump_world.tsv", "20160722_trump_acceptance.tsv", "20170228_trump_address.tsv"]
 	for fn in dev_fns:
